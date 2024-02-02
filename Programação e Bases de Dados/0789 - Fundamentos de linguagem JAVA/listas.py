39600ed85af142f9c64a92149ad8c611769c99ac
DanielQ <danieljorgerquaresma@gmail.com>
2024-02-02 22:53:05 +0000

    exercicios listas

---
+++ Programação e Bases de Dados/0789 - Fundamentos de linguagem JAVA/listas.py
@@ -0,0 +1,79 @@
+# exerc - 1
+#  listaint = []
+# for i in range(5):
+#     listaint.append(int(input("Insira um número: ")))
+
+# print(listaint)
+
+
+# exerc - 2
+# listaint = []
+# for i in range(10):
+#     listaint.append(float(input("Insira um número real: ")))
+# listaint.reverse()
+# print(listaint)
+
+# exerc - 3
+
+
+
+
+
+# exerc - 4
+listaint = []
+for i in range(10):
+    listaint.append(input("Insira um character: "))
+print(listaint.count('a') )
+listaint.count('e')
+listaint.count('i')
+listaint.count('o')
+listaint.count('u')
+
+print(listaint)
+
+
+
+
+
+
+
+# exerc - 5
+
+# exerc - 6
+
+# exerc - 7
+
+# exerc - 8
+
+# exerc - 9
+
+# exerc - 10
+
+# exerc - 11
+
+# exerc - 12
+
+# exerc - 13
+
+# exerc - 14
+
+# exerc - 15
+
+# exerc - 16
+
+# exerc - 17
+
+# exerc - 18
+
+# exerc - 19
+
+# exerc - 20
+
+# exerc - 21
+
+# exerc - 22
+
+# exerc - 23
+
+# exerc - 24
+
