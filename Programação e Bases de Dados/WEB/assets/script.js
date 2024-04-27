b2ed145f33e2b5c9158d2cc838dbf0d6f2897bec
DanielQ <danieljorgerquaresma@gmail.com>
2024-04-27 21:10:45 +0100

    final

---
+++ Programação e Bases de Dados/WEB/assets/script.js
@@ -0,0 +1,8 @@
+function escreverP (){
+    let novoParagrafo = prompt("Escreva o texto a adicionar :");
+    elemento = document.getElementById("saida").textContent;
+    document.getElementById("saida").innerHTML= elemento + novoParagrafo;
+}
+function apagarP() {
+    document.getElementById("saida").innerHTML = ""
+}
